c4d80884f79c67faf991ba3a9be431381adbca96
Guilherme <guilherme.chaves66@gmail.com>
2024-12-04 14:21:37 -0300

    :sparkles: convertendo-tipos-imputs-outputs

---
+++ imputs_outputs.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+print(f'Bem Vindo, {nome} {sobrenome}!')
+print(nome, sobrenome)
+print(nome, sobrenome, end="...\n")
+print(nome, sobrenome, sep="#")
+
+print(5//2)
